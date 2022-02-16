f6692345c31ed5cb8eb52cfd317142fa4cd90899
cheng <aj8c8c8@gmail.com>
2022-02-16 22:56:12 +0800

    BST remove

--- src/main/java/binary/search/tree/BinarySearchTreeLinkedList.java
+++ src/main/java/binary/search/tree/BinarySearchTreeLinkedList.java
@@ -1,5 +1,7 @@
 package binary.search.tree;
 
+import java.util.Optional;
+
 public class BinarySearchTreeLinkedList {
 
 	private Node root;
@@ -10,44 +12,108 @@ public class BinarySearchTreeLinkedList {
 			return;
 		}
 
-		insertByRecursion2(root, newData);
+//		insertByRecursion(root, newData);
+		insertByLoop(newData);
 	}
 
-	private void insertByRecursion(Node root, int newData) {
-
-		if (newData > root.getData()) {
-			if (root.getRight() == null) {
-				root.setRight(new Node(newData, null, null));
-				return;
-			}
-			insertByRecursion(root.getRight(), newData);
-		} else {
-			if (root.getLeft() == null) {
-				root.setLeft(new Node(newData, null, null));
-				return;
-			}
-			insertByRecursion(root.getLeft(), newData);
-		}
-
-	}
-
-	private Node insertByRecursion2(Node root, int newData) {
+	private Node insertByRecursion(Node root, int newData) {
 
 		if (root == null) {
 			return new Node(newData, null, null);
 		}
 
 		if (newData > root.getData()) {
-			root.setRight(insertByRecursion2(root.getRight(), newData));
+			root.setRight(insertByRecursion(root.getRight(), newData));
 		} else {
-			root.setLeft(insertByRecursion2(root.getLeft(), newData));
+			root.setLeft(insertByRecursion(root.getLeft(), newData));
+		}
+
+		return root;
+	}
+
+	private void insertByLoop(int newData) {
+
+		Node root = this.root;
+
+		while (true) {
+			if (newData > root.getData()) {
+				if (root.getRight() == null) {
+					root.setRight(new Node(newData, null, null));
+					break;
+				}
+				root = root.getRight();
+			} else {
+				if (root.getLeft() == null) {
+					root.setLeft(new Node(newData, null, null));
+					break;
+				}
+				root = root.getLeft();
+			}
+		}
+	}
+
+	public Node removeRec(Node root, int targetData) {
+
+		if (root == null) {
+			return root;
+		}
+
+		if (targetData > root.getData()) {
+			root.setRight(removeRec(root.getRight(), targetData));
+		} else if (targetData < root.getData()) {
+			root.setLeft(removeRec(root.getLeft(), targetData));
+		} else {
+			// leaf and one child
+			if (root.getLeft() == null && root.getRight() == null) {
+				return null;
+			} else if (root.getRight() == null) {
+				return root.getLeft();
+			} else if (root.getLeft() == null) {
+				return root.getRight();
+			} else {
+				// two child
+				Node minNode = findMin(root.getLeft(), targetData);
+				root.data = minNode.getData();
+				root.setLeft(removeRec(root.getLeft(), root.data));
+
+			}
+
 		}
 
 		return root;
 
 	}
 
-	private void clear() {
+	private Node findMin(Node root, int targetData) {
+
+		Node minNode = root;
+		while (minNode != null && minNode.getLeft() != null) {
+			minNode = minNode.getLeft();
+		}
+
+		return minNode;
+
+	}
+
+	public Optional<Node> find(int targetData) {
+		Node root = this.root;
+
+		while (root != null) {
+			if (targetData == root.getData()) {
+				return Optional.of(root);
+			}
+
+			if (targetData > root.getData()) {
+				root = root.getRight();
+			} else {
+				root = root.getLeft();
+			}
+		}
+
+		return Optional.empty();
+	}
+
+	public void clear() {
 		this.root = null;
 	}
 
--- src/test/java/binary/search/tree/BinarySearchTreeLinkedListTest.java
+++ src/test/java/binary/search/tree/BinarySearchTreeLinkedListTest.java
@@ -1,20 +1,17 @@
 package binary.search.tree;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import binary.search.tree.BinarySearchTreeLinkedList.Node;
 
 public class BinarySearchTreeLinkedListTest {
 
-	@BeforeEach
-	void printLine() {
-		System.out.println("=================================");
-	}
-
-	@Test
+//	@Test
 	void testInsert() {
 		BinarySearchTreeLinkedList binarySearchTree = new BinarySearchTreeLinkedList();
 		binarySearchTree.insert(10);
@@ -35,9 +32,47 @@ public class BinarySearchTreeLinkedListTest {
 		// L2
 		assertEquals(4, root.getLeft().getLeft().getData());
 		assertEquals(6, root.getLeft().getRight().getData());
-		
 		assertEquals(14, root.getRight().getLeft().getData());
 		assertEquals(16, root.getRight().getRight().getData());
 
+	}
+
+//	@Test
+	void find() {
+		BinarySearchTreeLinkedList binarySearchTree = new BinarySearchTreeLinkedList();
+		binarySearchTree.insert(10);
+		binarySearchTree.insert(5);
+		binarySearchTree.insert(15);
+
+		assertTrue(binarySearchTree.find(10).isPresent());
+		assertFalse(binarySearchTree.find(16).isPresent());
+	}
+
+	@Test
+	void remove() {
+		BinarySearchTreeLinkedList binarySearchTree = new BinarySearchTreeLinkedList();
+
+		// L0
+		binarySearchTree.insert(10);
+		// L1
+		binarySearchTree.insert(5);
+		binarySearchTree.insert(15);
+		// L2
+		binarySearchTree.insert(4);
+		binarySearchTree.insert(6);
+		binarySearchTree.insert(14);
+		binarySearchTree.insert(16);
+
+		Node root = binarySearchTree.getRootNode();
+//		binarySearchTree.removeRec(binarySearchTree.getRootNode(), 4);
+//		assertNull( root.getLeft().getLeft());
+//		
+//		
+		
+		binarySearchTree.removeRec(root, 15);
+		
+		assertEquals(14, root.getRight().getData());
+
+		assertNull(root.getRight().getLeft());
 	}
 }
